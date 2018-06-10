ecd37b9132ebe14e8ad5a525f5d67a51e934c027
tyleryoungblood <tyler@resimplifi.com>
2018-06-10 13:23:32 -0400

    wire up restangular cnxn to mlabs and delete dummy json data

--- scripts.js
+++ scripts.js
@@ -1,28 +1,44 @@
-var challengeApp = angular.module('challengeApp', ['ngRoute', 'ngGrid']);
+var challengeApp = angular.module('challengeApp', ['ngRoute', 'ngGrid', 'restangular']);
 
-// create the controller and inject Angular's $scope
-challengeApp.controller('mainController', function($scope) {
 
-    $scope.gridData = [{name: "Moroni", age: 50},
-                     {name: "Teancum", age: 43},
-                     {name: "Jacob", age: 27},
-                     {name: "Nephi", age: 29},
-                     {name: "Steve", age: 43},
-                     {name: "Scotty", age: 27},
-                     {name: "Maverick", age: 29},
-                     {name: "Enos", age: 34}];
-    $scope.gridOptions = {
-      data: 'gridData',
-      plugins: [new ngGridFlexibleHeightPlugin()]
-    };
+// mLabs resource url
+// mongodb://tyleryoungblood:/+nJ4uvL*<(8<Eq8@ds153980.mlab.com:53980/challenge-db
+
+challengeApp.constant('mongolab', {
+    url: 'https://api.mongolab.com/api/1/databases/challenge-db/collections/',
+    key: 'JiY3STffidCB_PDLbVoWiNotWTZpTTVQ'
 });
+
+// create the controller and inject Angular's $scope as well as Restangular
+challengeApp.controller('toolController', [
+    '$scope',
+    'Restangular',
+    function($scope, Restangular) {
+      var Tool = Restangular.all('tools');
+
+      $scope.gridData = Tool.getList().then(function (response) {
+          //strip extra object data out of response
+          $scope.gridData = response.plain();
+          console.log($scope.gridData);
+      });
+
+      //$scope.gridData = $scope.gridData.plain();
+      $scope.gridOptions = {
+        data: 'gridData',
+        columnDefs: [
+          {field:'name', displayName: 'Name'},
+          {field:'description', displayName:'Description', wordwrap: true, width: "*"}  ],
+        plugins: [new ngGridFlexibleHeightPlugin()]
+      };
+    }
+  ]);
 
 challengeApp.controller('editController', function($scope) {
     $scope.message = "This is the edit page.";
 });
 
 // create routes
-challengeApp.config(function($routeProvider, $locationProvider) {
+challengeApp.config(function($routeProvider, $locationProvider, RestangularProvider, mongolab) {
     $routeProvider
 
         .when('/', {
@@ -41,6 +57,16 @@ challengeApp.config(function($routeProvider, $locationProvider) {
 
     //remove the hash prefix from the URL for pretty URLs
     $locationProvider.hashPrefix('');
+
+    //configure Restangular connection to mLabs
+    RestangularProvider.setBaseUrl(mongolab.url);
+    RestangularProvider.setDefaultRequestParams({
+      apiKey: mongolab.key
+    });
+    //configure id to match mLabs format
+    RestangularProvider.setRestangularFields({
+      id: '_id.$oid'
+    });
 
 
 
